d0b7959ed8c1a11f1d3350689820ba9b65626aea
paul_vonhagen <pvonhagen26@gmail.com>
2020-09-21 18:58:34 -0700

    updated db and schema with card and deck models

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_22_005739) do
+ActiveRecord::Schema.define(version: 2020_09_22_011235) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cards", force: :cascade do |t|
+    t.string "question"
+    t.string "answer"
+    t.integer "deck_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "decks", force: :cascade do |t|
     t.string "name"
