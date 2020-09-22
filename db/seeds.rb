811dd679060b770d26d1913819dddb8de5fd5810
paul_vonhagen <pvonhagen26@gmail.com>
2020-09-21 18:17:30 -0700

    built Card model and basic seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,3 +2,6 @@ jscript = Category.create(name: "Javascript")
 
 ruby = Category.create(name: "Ruby")
 
+jsq1 = Card.create(question: "What does js stand for?", answer: "JavaScript", deck_id: jscript.id)
+
+rubyq1 = Card.create(question: "What is the full MVC Ruby framework called?", answer: "Ruby on Rails", deck_id: ruby.id)
