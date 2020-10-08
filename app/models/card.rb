8bc7393f34e766daa8d6d866f7b2cda9f21b2912
paul_vonhagen <pvonhagen26@gmail.com>
2020-10-07 19:06:41 -0700

    refactored Card class validation list

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,6 +1,4 @@
 class Card < ApplicationRecord
     belongs_to :deck
-    validates :question, presence: true
-    validates :answer, presence: true
-    validates :deck_id, presence: true
+    validates :question, :answer, :deck_id, presence: true
 end
